2f713730c927041faa9259024b99f7678f4c9b0b
Career404 <105638831+Career404@users.noreply.github.com>
2023-02-01 21:53:46 +0400

    feat: just kidding (add Joke component)

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { useState } from 'react'
 import './App.css'
 import Bored from './components/Bored'
+import Joke from './components/Joke'
 
 function App() {
 	return (
 		<div className="App">
+			<Joke />
 			<Bored />
 		</div>
 	)
--- src/components/Joke.tsx
+++ src/components/Joke.tsx
@@ -1,1 +1,35 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+
+//TODO: style: fix the Joke component on top, make the text larger, delivery in linear gradient
+
+export default function Joke() {
+	const [joke, setJoke] = useState({
+		error: true,
+		joke: 'Sorry, something went wrong! Try to reload the window (and see the API response in the console)',
+		setup: '',
+		delivery: '',
+	})
+	useEffect(() => {
+		loadNewJoke()
+	}, [])
+	async function loadNewJoke() {
+		const request = await fetch(
+			`https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,racist,sexist,explicit`
+		)
+		const jokeResponse = await request.json()
+		console.log(jokeResponse)
+		return setJoke(jokeResponse)
+	}
+	return (
+		<>
+			<div className="jokebox">
+				<p>
+					Joke of the session: <br />
+					{joke.joke}
+					{joke.setup}
+					{joke.delivery}
+				</p>
+			</div>
+		</>
+	)
+}
